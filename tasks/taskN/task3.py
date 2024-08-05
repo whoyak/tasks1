4960d1388ea86e967038cddc0e5c2238e0a59c7a
whoyak <73415603+whoyak@users.noreply.github.com>
2024-08-05 20:13:01 +0300

    Add files via upload

---
+++ tasks/taskN/task3.py
@@ -0,0 +1,7 @@
+#3rd programm
+result = 2*2+2
+
+result2=(2*2)+2
+
+result_bool= result==result2
+print(result_bool)
---
+++ tasks/taskN/task4.py
@@ -0,0 +1,6 @@
+#4th programm
+str = '123.456'
+num = float(123.456)
+num = num*10
+num = int(num)
+print(num%10)
